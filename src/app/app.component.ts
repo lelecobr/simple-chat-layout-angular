25f7ff12f2ceed222be52c90babfe0f9cf962dbf
Leandro Dias <leandro.dias97@cs.cruzeirodosul.edu.br>
2024-02-14 19:47:38 -0300

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonComponent } from './compontens/person/person.component';
+import { MessageComponent } from './compontens/message/message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
