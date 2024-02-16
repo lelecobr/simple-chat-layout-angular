b690cb5ea677babc4a4f107ac3201f3fda94b722
Leandro Dias <leandro.dias97@cs.cruzeirodosul.edu.br>
2024-02-15 21:41:07 -0300

    input style finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonComponent } from './compontens/person/person.component';
 import { MessageComponent } from './compontens/message/message.component';
+import { InputComponent } from './compontens/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PersonComponent, MessageComponent],
+  imports: [RouterOutlet, PersonComponent, MessageComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
